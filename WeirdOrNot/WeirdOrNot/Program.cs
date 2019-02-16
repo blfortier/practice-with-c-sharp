5ad0f660b4a86ba597d4784e84d184adb5ef92d8
Brittney Fortier <brittfort34@gmail.com>
2019-02-15 20:23:56 -0500

    A program that displays a message depending on the number entered by user.

---
+++ WeirdOrNot/WeirdOrNot/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WeirdOrNot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            if (N % 2 == 1)
+                Console.WriteLine("Weird");
+            else
+            {
+                if (N >= 2 && N <= 5)
+                    Console.WriteLine("Not Weird");
+                else if (N >= 6 && N <= 20)
+                    Console.WriteLine("Weird");
+                else
+                    Console.WriteLine("Not Weird");
+            }
+
+
+        }
+    }
+}
