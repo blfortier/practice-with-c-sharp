e13aa7802750b3cc5d680f7cca4abd49b8849409
Brittney Fortier <brittfort34@gmail.com>
2019-02-17 13:31:04 -0500

    Created a program that acts as a stopwatch.

---
+++ StopwatchExercise/StopwatchExercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace StopwatchExercise
+{
+    class Program
+    {
+        static void Main(string[] args)     
+        {
+            // Instantiate a new stopwatch object
+            var stopwatch = new Stopwatch();
+
+            // Start and stop the stopwatch
+            for (var i = 0; i < 2; i++)
+            {
+                stopwatch.Start();
+
+                Thread.Sleep(1000);
+
+                stopwatch.Stop();
+            }            
+
+            // Print how long the stopwatch was running 
+            Console.WriteLine("Duration is {0}", stopwatch.Duration());
+        }
+    }
+}
---
+++ StopwatchExercise/StopwatchExercise/Stopwatch.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StopwatchExercise
+{
+    public class Stopwatch
+    {
+        private DateTime _start;
+        private DateTime _stop;
+        private bool _isRunning;
+
+        // Calling the Start method will start the 
+        // stopwatch by calling DateTime.Now
+        public void Start()
+        {
+            // If the stopwatch is already running,
+            // throw an exception
+            if (_isRunning)
+            {
+                throw new InvalidOperationException("The stopwatch is already running...");
+            }
+
+            _start = DateTime.Now;
+            _isRunning = true;
+        }
+
+        // The Stop method calls DateTime.Now and this is
+        // the stop time
+        public void Stop()
+        {
+            if (!_isRunning)
+            {
+                throw new InvalidOperationException("The stopwatch isn't running...");
+            }
+
+            _stop = DateTime.Now;
+            _isRunning = false;
+
+        }
+
+        // The Stop method
+        public TimeSpan Duration()
+        {
+            return _stop - _start;
+        }
+    }
+}
