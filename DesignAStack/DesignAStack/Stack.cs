e1851a7b5b55eadaf42e45db1d806321c6af3dd0
Brittney Fortier <brittfort34@gmail.com>
2019-02-20 12:32:01 -0500

    Created a stack to store and remove objects

---
+++ DesignAStack/DesignAStack/Stack.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DesignAStack
+{
+    public class Stack
+    {
+        private readonly List<object> _stack = new List<object>();
+
+        public void Push(object obj)
+        {
+            if (obj == null)
+                throw new InvalidOperationException("No null objects can be passed.");
+
+            _stack.Add(obj);
+
+        }
+
+        public object Pop()
+        {
+            if (_stack.Count == 0)
+                throw new InvalidOperationException("No objects to remove");
+
+            var lastItem = _stack.Count - 1;
+
+            var returnObject = _stack[lastItem];
+
+            _stack.RemoveAt(lastItem);
+
+            return (returnObject);
+        }
+
+        public void Clear()
+        {
+            _stack.Clear();
+        }
+      
+
+    }
+}
