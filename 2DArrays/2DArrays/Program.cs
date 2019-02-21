2537f1c35f4b231ea154b06dc384f13fbb102bae
Brittney Fortier <brittfort34@gmail.com>
2019-02-21 13:24:53 -0500

    A program where a Student class inherits from a Person class. The average of student test scores is found and returned to the user.

---
+++ 2DArrays/2DArrays/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _2DArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] arr = new int[6][];
+            int temp;
+            int max = 0;
+
+            for (int i = 0; i < 6; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp)); 
+            }
+
+            for (int row = 0; row < 4; row++)
+                {
+                    for (int col = 0; col < 4; col++)
+                    {
+                        temp = arr[row][col] + arr[row][col + 1] + arr[row][col + 2] + arr[row + 1][col + 1] + arr[row + 2][col] + arr[row + 2][col + 1] + arr[row + 2][col + 2];
+
+                        if (max < temp)
+                            max = temp;
+                    }
+                }
+
+                Console.WriteLine(max);
+        }
+    }
+}
