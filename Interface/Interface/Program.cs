7d532f2bd17ef5730b40289cbd7f8e2b6fcdd53d
Brittney Fortier <brittfort34@gmail.com>
2019-02-28 08:37:31 -0500

    Implements an interface to sum all divisors of the integer entered by the user.

---
+++ Interface/Interface/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Interface
+{
+    public interface AdvancedArithmetic
+    {
+        int divisorSum(int n);
+    }
+
+    public class Calculator : AdvancedArithmetic
+    {
+        private int sum = 0;
+        public int divisorSum(int n)
+        {
+            for (var i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                    sum += i;
+            }  
+  
+            return sum;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter an integer: ");
+            int n = Int32.Parse(Console.ReadLine());
+            AdvancedArithmetic myCalculator = new Calculator();
+            int sum = myCalculator.divisorSum(n);
+            Console.WriteLine("I implemented: AdvancedArithmetic\n" + sum);
+        }
+
+    }
+}
