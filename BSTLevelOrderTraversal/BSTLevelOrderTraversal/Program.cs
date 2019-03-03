5e0aa1ce580ef90e7e1ca8eba49718ee7a122c9f
Brittney Fortier <brittfort34@gmail.com>
2019-03-03 14:06:50 -0500

    Implemented the LevelOrder method which prints the level-order traversal of the BST.

---
+++ BSTLevelOrderTraversal/BSTLevelOrderTraversal/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BSTLevelOrderTraversal
+{
+    class Node
+    {
+        public Node left, right;
+        public int data;
+        public Node(int data)
+        {
+            this.data = data;
+            left = right = null;
+        }
+    }
+
+    class Solution
+    {
+
+        static void LevelOrder(Node root)
+        {
+            //Write your code here
+
+            if (root == null)
+                return;
+
+            Queue<Node> nodeQueue = new Queue<Node>();
+            nodeQueue.Enqueue(root);
+
+            while (nodeQueue.Count != 0)
+            {
+                var currentNode = nodeQueue.Dequeue();
+                Console.Write(currentNode.data + " ");
+
+                if (currentNode.left != null)
+                    nodeQueue.Enqueue(currentNode.left);
+                if (currentNode.right != null)
+                    nodeQueue.Enqueue(currentNode.right);
+            }
+        }
+        static Node Insert(Node root, int data)
+        {
+            if (root == null)
+            {
+                return new Node(data);
+            }
+            else
+            {
+                Node curr;
+                if (data <= root.data)
+                {
+                    curr = Insert(root.left, data);
+                    root.left = curr;
+                }
+                else
+                {
+                    curr = Insert(root.right, data);
+                    root.right = curr;
+                }
+                return root;
+            }
+        }
+
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            Node root = null;
+            int T = Int32.Parse(Console.ReadLine());
+            while (T-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                root = Insert(root, data);
+            }
+            LevelOrder(root);
+
+        }
+        }
+    }
+}
--- TicTacToeGame/TicTacToeGame/Program.cs
+++ TicTacToeGame/TicTacToeGame/Program.cs
@@ -8,80 +8,181 @@ namespace TicTacToeGame
 {
     class Program
     {
-        static void Main(string[] args)
+        class TicTacToe
         {
-            Dictionary<int, string> board = new Dictionary<int, string>()
+            protected char[] board;
+            protected char player1Token;
+            protected int[] player1Moves;
+            protected char player2Token;
+            protected int[] player2Moves;
+            protected char winner;
+            protected char currentToken;
+            private int moveCount = 0;
+
+            public TicTacToe(char player1, char player2)
             {
-                {1, " "},
-                {2, " "},
-                {3, " "},
-                {4, " "},
-                {5, " "},
-                {6, " "},
-                {7, " "},
-                {8, " "},
-                {9, " "}
-            };
-          
-
-            DisplayBoard(board);
-
-            var moveCount = 0;
-            var import = new[] {"X", "X", "O", "X", "O", "O", "X", "O", "X"};
-
-            ImportBoard(import, board);
-
-            // Console.WriteLine(board[7]);
-            // if(board.Contains(new KeyValuePair<int, string>(4, null)))
-            //      Console.WriteLine("Sorry, that spot is taken. Try another cell.");             
-            //  board[4] = "X";
-            //  Console.WriteLine(board[4]);     
-
-        }
-
-        public static void ImportBoard(string[] moves, Dictionary<int, string> board)
-        {
-            List<int> player1Moves = new List<int>();
-            List<int> player2Moves = new List<int>();
-
-            for (var i = 0; i < moves.Length; i++)
-            {
-                board[i + 1] = moves[i];
-
-                if (i % 2 == 0)
-                    player1Moves.Add(Array.IndexOf(moves, moves[i]));
-                else
-                    player2Moves.Add(Array.IndexOf(moves, moves[i]));
+                this.player1Token = player1;
+                this.player2Token = player2;
+                this.winner = ' ';
+                this.board = SetBoard();
             }
 
-            foreach (KeyValuePair<int, string> item in board)
-                Console.WriteLine("Cell: {0}, Token: {1} ", item.Key, item.Value);
+            private static char[] SetBoard()
+            {
+                char[] board = new char[9];
+                for (int i = 0; i < board.Length; i++)
+                    board[i] = ' ';
 
-            foreach (var move in player1Moves)
-                Console.WriteLine("Player 1 moves: {0}", player1Moves);
+                return board;
+            }
 
-            foreach (var move in player2Moves)
-                Console.WriteLine("Player 2 moves: {0}", player2Moves);
+            public bool IsValidMove(int cell)
+            {
+                bool isValid = WithinBoard(cell) && !CellTaken(cell);
+
+                if (isValid)
+                {
+                    board[cell - 1] = currentToken;
+                    currentToken = (currentToken == player1Token) ? player2Token : player1Token;
+                }
+
+                return isValid;
+            }
+
+            private bool CellTaken(int cell)
+            {
+                return board[cell - 1] != ' ';
+            }
+
+            private bool WithinBoard(int cell)
+            {
+                return cell > 0 && cell < board.Length + 1;
+            }
+
+            public void DisplayBoard()
+            {
+                var topAndBottom = "     |     |    ";
+                var connector = "     |     |    \n-----+-----+-----\n     |     |    ";
+
+                Console.WriteLine();
+                Console.WriteLine(topAndBottom);
+                Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[0], board[1], board[2]);
+                Console.WriteLine(connector);
+                Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[3], board[4], board[5]);
+                Console.WriteLine(connector);
+                Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[6], board[7], board[8]);
+                Console.WriteLine(topAndBottom);
+                Console.WriteLine();
+            }
+
+            public void DisplayHelpBoard()
+            {
+                var topAndBottom = "     |     |    ";
+                var connector = "     |     |    \n-----+-----+-----\n     |     |    ";
+
+                Console.WriteLine();
+                Console.WriteLine(topAndBottom);
+                Console.WriteLine("  1  |  2  |  3  ");
+                Console.WriteLine(connector);
+                Console.WriteLine("  4  |  5  |  6  ");
+                Console.WriteLine(connector);
+                Console.WriteLine("  7  |  8  |  9  ");
+                Console.WriteLine(topAndBottom);
+                Console.WriteLine();
+
+            }
+
+            public void ImportBoard(string importedBoard)
+            {
+              //  var arrayOfBoardMoves = (importedBoard.Split(','));
+              //char[] arrayOfBoardMoves = importedBoard.Trim().ToCharArray();
+
+            foreach (var VARIABLE in importedBoard.Split(' '))
+                {
+                    Console.WriteLine(VARIABLE);   
+                }
+
+                for (var i = 0; i < board.Length; i++)
+                {
+                   // this.board[i] = arrayOfBoardMoves[i];
+                }
+            }
 
 
+            public bool IsThereAWin()
+            {
+                bool DiagonalsAndMiddles =
+                    FirstDiagonal() || SecondDiagonal() || SecondRow() || SecondColumn() && board[4] != ' ';
+                bool FirstRowAndColumn = FirstRow() || FirstColumn() && board[0] != ' ';
+                bool ThirdRowAndColumn = ThirdRow() || ThirdColumn() && board[8] != ' ';
 
-        }
+                if (DiagonalsAndMiddles)
+                    this.winner = board[4];
+                else if (FirstRowAndColumn)
+                    this.winner = board[0];
+                else if (ThirdRowAndColumn)
+                    this.winner = board[8];
 
-        public static void DisplayBoard(Dictionary<int, string> board)
+                return DiagonalsAndMiddles || FirstRowAndColumn || ThirdRowAndColumn;
+            }
+
+          
+            private bool FirstDiagonal()
+            {
+                return board[0] == board[4] && board[4] == board[8];
+            }
+
+            private bool SecondDiagonal()
+            {
+                return board[2] == board[4] && board[4] == board[6];
+            }
+
+
+            private bool SecondColumn()
+            {
+                return board[1] == board[4] && board[4] == board[7];
+            }
+
+            private bool SecondRow()
+            {
+                return board[3] == board[4] && board[4] == board[5];
+            }
+
+            private bool FirstRow()
+            {
+                return board[0] == board[1] && board[1] == board[2];
+            }
+
+            private bool FirstColumn()
+            {
+                return board[0] == board[3] && board[3] == board[6];
+            }
+
+            private bool ThirdColumn()
+            {
+                return board[2] == board[5] && board[5] == board[8];
+            }
+
+            private bool ThirdRow()
+            {
+                return board[6] == board[7] && board[7] == board[8];
+            }
+        }   
+
+        static void Main(string[] args)
         {
-            var topAndBottom = "     |     |    ";
-            var connector = "     |     |    \n-----+-----+-----\n     |     |    ";
+            var game = new TicTacToe('O', 'X');
+            Console.WriteLine("Board");
+            game.DisplayBoard();
+            
+            var play = "X, X, O, O, X, O, X, O, X";
+            game.ImportBoard(play);
+            game.DisplayBoard();
 
-            Console.WriteLine();
-            Console.WriteLine(topAndBottom);
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[1], board[2], board[3]);
-            Console.WriteLine(connector);
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[4], board[5], board[6]);
-            Console.WriteLine(connector);
-            Console.WriteLine("  {0}  |  {1}  |  {2}  ", board[7], board[8], board[9]);
-            Console.WriteLine(topAndBottom);
 
-            Console.WriteLine();
+
+            // game.DisplayHelpBoard();
         }
     }
+
 }
