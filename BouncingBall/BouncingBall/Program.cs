7b1f5c0a8fc80ba7d96eac26e5c0654fe0007876
Brittney Fortier <brittfort34@gmail.com>
2019-03-04 12:54:37 -0500

    Returns how many times a ball dropped from above a window, will bounce past that window.

---
+++ BouncingBall/BouncingBall/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BallBounce
+{
+    public class BouncingBall
+    {
+        public static int bouncingBall(double height, double bounceRate, double windowHeight)
+        {
+            if (height < 0 || height < windowHeight || bounceRate <= 0 || bounceRate >= 1)
+                return -1;
+
+            // Make sure the first initial drop past
+            // the window is counted
+            var bounce = 1;
+
+            do
+            {
+                height = height * bounceRate;
+                bounce++;
+
+            } while (height > windowHeight);
+
+            return bounce;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Height of floor ball was dropped from: ");
+            var throwHeight = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Bounce rate: ");
+            var rateOfBounce = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Window height: ");
+            var window = Convert.ToDouble(Console.ReadLine());
+
+            var numBounces = BouncingBall.bouncingBall(throwHeight, rateOfBounce, window);
+            Console.WriteLine(numBounces);
+        }
+    }
+}
---
+++ PrimeNumbers/PrimeNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrimeNumbers
+{
+  
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number of test cases: ");
+            var input = Convert.ToInt32(Console.ReadLine());
+
+            string result;
+
+            while (input-- > 0)
+            {
+                int data = Int32.Parse(Console.ReadLine());
+                result = IsPrime(data);
+                Console.WriteLine(result);
+
+            }
+
+        }
+
+        private static string IsPrime(int data)
+        {
+            string message;
+            bool isPrime;
+
+            if (data == 1)
+                isPrime = false;
+            if (data == 2)
+                isPrime = true;
+            
+            for (int i = 3; i < data; i++)
+            {
+                if (data % i == 0)
+                    isPrime = false;
+            }
+
+            isPrime = true;
+
+
+            return message = isPrime ? "Prime" : "Not prime";
+        }
+    }
+}
