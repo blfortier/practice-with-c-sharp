460aeefccc800da3a3385119619cf0f035c91010
Brittney Fortier <brittfort34@gmail.com>
2019-03-05 19:08:48 -0500

    Prints out the contents of an integer array in reverse order.

--- BouncingBall/BouncingBall/BouncingBall.cs
+++ BouncingBall/BouncingBall/BouncingBall.cs
@@ -4,24 +4,22 @@
     {
         public static int bouncingBall(double height, double bounceRate, double windowHeight)
         {
-            // Originally, this was set to 1, but 
-            // then my expected output was 2 over
+
             int windowPass = -1;
 
             if (height < 0 || height < windowHeight || bounceRate <= 0 || bounceRate >= 1)
                 return windowPass;
             do
             {
-                height = height * bounceRate;
+                height *= bounceRate;
                 // windowPass needs to be incremented by 
                 // 2 in order to account for the mom seeing the
-                // ball pass by the window! 
+                // ball pass by the window
                 windowPass += 2;
 
             } while (height > windowHeight);
 
             return windowPass;
         }
-    }
 }
-
+}
---
+++ ReverseAnArray/ReverseAnArray/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReverseAnArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many numbers in the array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter numbers seperated by a space: ");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            for (var i = n - 1; i >= 0; i--)
+                Console.Write("{0} ", arr[i]);
+            Console.WriteLine();
+
+        }
+    }    
+}
