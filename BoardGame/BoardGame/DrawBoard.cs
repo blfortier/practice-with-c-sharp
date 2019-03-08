79552a56eec2fd985a5f5454795faa91e06107d9
Brittney Fortier <brittfort34@gmail.com>
2019-03-08 15:15:52 -0500

    Creates a game board based on the height and width passed by the user.

---
+++ BoardGame/BoardGame/DrawBoard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoardGame
+{
+    public class Board
+    {
+        private int _height;
+        private int _width;        
+        
+        public Board(int height, int width)
+        {
+            this._height = height;
+            this._width = width;
+
+            DrawBoard(height, width);
+        }
+
+        public void DrawBoard(int height, int width)
+        {
+            int j;
+           
+            for (var i = 0; i < height; i++)       
+            {
+                Console.Write("|  ");
+                
+                for (j = 0; j < width; j++)
+                {
+                    Console.Write("|  ");                      
+                }
+
+                if(j == width)
+                {
+                    Console.WriteLine();
+                    for (var t = 0; t < width; t++)
+                        Console.Write("+--");
+                }
+
+                Console.Write("+");
+                Console.WriteLine();             
+            }
+
+            Console.WriteLine();
+
+         }
+    }
+}
+
---
+++ BoardGame/BoardGame/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BoardGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var height = 7;
+            var width = 9;
+
+            var game = new Board(height, width);
+        }       
+    }
+}
+
