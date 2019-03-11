64500be7f3f95037567d90c81bde2a27b2bcb36b
Brittney Fortier <brittfort34@gmail.com>
2019-03-10 20:24:26 -0400

    Calculates the total cost of a meal, including tip and tax.

---
+++ MealCost/MealCost/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MealCost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Meal cost: ");
+            double meal_cost = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Tip percent: ");
+            int tip_percent = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Tax percent: ");
+            int tax_percent = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Total cost of meal: ");
+            solve(meal_cost, tip_percent, tax_percent);
+        }
+
+        static void solve(double meal_cost, int tip_percent, int tax_percent)
+        {
+            double tip_cost = (meal_cost * tip_percent) / 100;
+            double tax_cost = (meal_cost * tax_percent) / 100;
+            double total_cost = Math.Round(meal_cost + tip_cost + tax_cost);
+            Console.WriteLine(total_cost);
+        }
+
+    }
+}
---
+++ RegEx/RegEx/Program.cs
@@ -0,0 +1,65 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+namespace RegEx
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many entries? ");
+            int N = Convert.ToInt32(Console.ReadLine());
+
+            // var containsGmail = new List<string>();
+            var namesAndEmails = new Dictionary<string, string>();
+
+            for (int NItr = 0; NItr < N; NItr++)
+            {
+                Console.WriteLine("Format: Name Email");
+                string[] firstNameEmailID = Console.ReadLine().Split(' ');
+
+                string firstName = firstNameEmailID[0];           
+                string emailID = firstNameEmailID[1];
+
+                namesAndEmails.Add(firstName, emailID);
+
+                //if (IsMatch(emailID, firstName))
+                //{
+                //    containsGmail.Add(firstName);
+                //    Console.WriteLine("name added...");
+                //}
+            }
+                      
+
+            foreach (var name in namesAndEmails)
+            {
+                Console.WriteLine(name);
+            }
+
+        }
+
+        public static bool IsMatch(string email, string name)
+        {
+            Regex reg = new Regex(@"/(@)([gmail])\w+/g");
+
+            Match result = reg.Match(email);
+            if (result.Success)
+            {
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
