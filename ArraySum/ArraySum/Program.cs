10810b998d95159f524ba8e6f2891caf27bd053e
Brittney Fortier <brittfort34@gmail.com>
2019-03-13 12:19:27 -0400

    Sums the integers in an array

---
+++ ArraySum/ArraySum/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ArraySum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number of ints in array: ");
+            var arrLength = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Array element: ");
+
+            var array = new int[arrLength];
+            var sum = 0;
+
+            for (var i = 0; i < arrLength; i++)
+            {
+                var number = Convert.ToInt32(Console.ReadLine());
+                array[i] = number;
+                sum += number;
+            }
+
+            Console.WriteLine("Sum of the array is: {0}", sum);
+        }
+       
+    }
+}
