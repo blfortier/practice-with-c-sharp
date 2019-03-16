182071f58214ab19a27c2c3716324a328c44f753
Brittney Fortier <brittfort34@gmail.com>
2019-03-16 19:33:41 -0400

    Accepts an integer and returns the reversed digits, no strings

---
+++ Kata/Kata/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClassChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Integer: ");
+            var number = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The integer reversed is: {0}\n", ReverseOrder(number));          
+        }
+
+        public static int ReverseOrder(int num)
+        {
+            int reversed = 0;
+            
+            while (num != 0)
+            {
+                reversed = reversed * 10 + num % 10;
+                num = num / 10;
+            }                       
+
+            return reversed;
+        }
+    }
+}
