87934f164651a2d96d2491a264027a79b9c480e3
Brittney Fortier <brittfort34@gmail.com>
2019-03-19 14:45:57 -0400

    Returns the absolute difference of the sums of the matrix diagonals.

---
+++ SquareMatrx/SquareMatrx/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace SquareMatrx
+{
+    class Program
+    {
+        static int DiagonalDifference(int[][] arr)
+        {
+
+            int sumOfLeftDiagonal = arr[0][0] + arr[1][1] + arr[2][2];
+            Console.WriteLine(sumOfLeftDiagonal);
+
+            int sumOfRightDiagonal = arr[0][2] + arr[1][1] + arr[2][0];
+
+            return Math.Abs(sumOfLeftDiagonal - sumOfRightDiagonal);
+        }
+
+        static void Main(string[] args)
+        {
+           // TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[][] arr = new int[n][];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+            }
+
+            int result = DiagonalDifference(arr);
+
+            //textWriter.WriteLine(result);
+
+            //textWriter.Flush();
+            //textWriter.Close();
+        }
+    }
+    
+}
