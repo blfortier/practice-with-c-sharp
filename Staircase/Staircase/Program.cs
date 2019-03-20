9832f15b34b548eacc5235977b5f2a2ef5ab4179
Brittney Fortier <brittfort34@gmail.com>
2019-03-20 15:18:50 -0400

    Prints a staircase of n height, n entered by the user.

---
+++ Staircase/Staircase/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Staircase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Size of staircase: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Staircase(n);
+        }
+
+        private static void Staircase(int n)
+        {
+            for (int i = 0; i <= n; i++)
+            {
+                for (int k = 0; k <= n - 1 - i; k++)
+                    Console.Write(" ");
+                for (int j = 0; j < i + 1 + 1; j++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
