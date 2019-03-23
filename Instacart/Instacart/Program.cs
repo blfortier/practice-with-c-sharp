49a268311eb04368e606dc429d3602e2424312d6
Brittney Fortier <brittfort34@gmail.com>
2019-03-22 20:18:10 -0400

    Determines if shoppers should take an Instacart delivery job.

---
+++ Instacart/Instacart/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Instacart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] order = new int[] { 200, 20, 15 };
+            int[] order1 = { 300, 40, 5 };
+            int[] order2 = { 600, 40, 10 };
+            int[][] shoppers = new int[][] { order1, order2 };
+
+            bool[] shouldDeliver = delivery(order, shoppers);
+
+            foreach  (bool result in shouldDeliver)
+                Console.WriteLine(result);
+        }
+
+        static bool[] delivery(int[] order, int[][] shoppers)
+        {
+
+            var takeOrder = new bool[shoppers.Length];
+
+            int storeToCustomerDist = order[0];
+            int customerReadyTime = order[1];
+            int customerMaxWaitTime = order[2];
+            int fufillOrder;
+
+            int shopperDistFromStore;
+            int shopperSpeed;
+            int shopperTimeSpent;
+
+            for (var i = 0; i < shoppers.Length; i++)
+            {
+                shopperDistFromStore = shoppers[i][0];
+                shopperSpeed = shoppers[i][1];
+                shopperTimeSpent = shoppers[i][2];
+
+                fufillOrder = (storeToCustomerDist + shopperDistFromStore) / shopperSpeed + shopperTimeSpent;
+
+                if (fufillOrder < customerReadyTime)
+                    takeOrder[i] = false;
+                else if (fufillOrder < customerReadyTime + customerMaxWaitTime)
+                    takeOrder[i] = true;
+            }
+
+            return takeOrder;
+        }
+
+    }
+}
--- Uber/Uber/Program.cs
+++ Uber/Uber/Program.cs
@@ -9,24 +9,25 @@ namespace Uber
     {
         static void Main(string[] args)
         {
-            double[] costPerMin = new double[] { 0.2, 0.35, 0.4, 0.45 };
-            double[] costPerMile = new double[] { 1.1, 1.8, 2.3, 3.5 };
+            double[] costPerMin = { 0.2, 0.35, 0.4, 0.45 };
+            double[] costPerMile = { 1.1, 1.8, 2.3, 3.5 };
 
             var rideData = new UberRates(30, 7, costPerMin, costPerMile);
+            Console.WriteLine(rideData.ride_time);
+            rideData.ride_time = 35;
+            Console.WriteLine(rideData.ride_time);
             
             foreach (double cost in rideData.CalculateRates(rideData))
-            {
                 Console.WriteLine(cost);
-            }
         }
     }
 
     public class UberRates
     {
-        public int ride_time { get; set;}
-        public int ride_distance { get; set; }
-        public double[] cost_per_minute { get; set; }
-        public double[] cost_per_mile { get; set; }
+        private int _ride_time;
+        private int _ride_distance;
+        private double[] _cost_per_minute;
+        private double[] _cost_per_mile;
 
         public UberRates()
         {
@@ -34,20 +35,18 @@ namespace Uber
 
         public UberRates(int rideTime, int rideDist, double[] costPerMin, double[] costPerMile )
         {
-            this.ride_time = rideTime;
-            this.ride_distance = rideDist;
-            this.cost_per_minute = costPerMin;
-            this.cost_per_mile = costPerMile;
+            this._ride_time = rideTime;
+            this._ride_distance = rideDist;
+            this._cost_per_minute = costPerMin;
+            this._cost_per_mile = costPerMile;
         }
 
         public double[] CalculateRates(UberRates data)
         {
-            double[] cost = new double[cost_per_minute.Length];
+            double[] cost = new double[_cost_per_minute.Length];
 
-            for (int i = 0; i < cost_per_minute.Length; i++)
-            {
-                cost[i] = (cost_per_minute[i] * ride_time) + (cost_per_mile[i] * ride_distance);
-            }
+            for (int i = 0; i < _cost_per_minute.Length; i++)
+                cost[i] = (_cost_per_minute[i] * _ride_time) + (_cost_per_mile[i] * _ride_distance);
 
             return cost;
         }
