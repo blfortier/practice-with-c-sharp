050fa30404000a86ebfb1dab351fb04fdec3dce5
Brittney Fortier <brittfort34@gmail.com>
2019-03-25 14:53:35 -0400

    Implemented a RatingThreshold method that accepts a double and an int array. Calculates which professional needs a review based the average of their ratings and the given threshold.

---
+++ Thumbtack/Thumbtack/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Thumbtack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        int[] RatingThreshold(double threshold, int[][] ratings)
+        {
+
+            double[] arrOfAverages = new double[ratings.Length];
+            List<int> results = new List<int>();
+
+            for (var i = 0; i < ratings.Length; i++)
+            {
+
+                arrOfAverages[i] = ratings[i].Average();
+
+                if (arrOfAverages[i] < threshold)
+                    results.Add(i);
+            }
+
+            return results.ToArray();
+        }
+
+    }
+}
