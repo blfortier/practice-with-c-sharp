4b921f47f55d0450a5613cb738c6034ad39fcb9b
Brittney Fortier <brittfort34@gmail.com>
2019-04-03 14:16:41 -0400

    Updated BirthdayCakeCandles method.

--- BirthdayCandles/BirthdayCandles/Program.cs
+++ BirthdayCandles/BirthdayCandles/Program.cs
@@ -12,10 +12,12 @@ namespace BirthdayCandles
         {
             TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
 
+            Console.Write("How many candles? ");
             int arCount = Convert.ToInt32(Console.ReadLine());
 
-            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
-            ;
+            Console.WriteLine("Enter space seperated integers, representing candle height");
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            
             int result = BirthdayCakeCandles(ar);
 
             textWriter.WriteLine(result);
@@ -26,12 +28,19 @@ namespace BirthdayCandles
 
         static int BirthdayCakeCandles(int[] ar)
         {
-            int max = ar.Max();
+            // int max = ar.Max();
+            int max = 0;
             int count = 0;
 
             for (int i = 0; i < ar.Length; i++)
             {
-                if (ar[i] == max)
+                if (ar[i] > max)
+                    max = ar[i];
+            }
+
+            foreach (var item in ar)
+            {
+                if (item == max)
                     count++;
             }
 
