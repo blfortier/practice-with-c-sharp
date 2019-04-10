1f6444a26cccf40ea0b6379401a5351291290c66
Brittney Fortier <brittfort34@gmail.com>
2019-04-10 19:21:03 -0400

    Given an array of integers representing the color of a sock, determines how many pairs of socks with matching colors there are.

---
+++ MatchingSocks/MatchingSocks/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatchingSocks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp));
+            int result = SockMerchant(n, ar);
+
+            Console.WriteLine(result);
+        }
+
+        public static int SockMerchant(int n, int[] ar)
+        {
+            int numberOfPairs = 0;
+            List<int> pairs = new List<int>();
+
+            for (int i = 0; i < ar.Length; i++)
+            {
+                if (!pairs.Contains(ar[i]))
+                    pairs.Add(ar[i]);
+                else
+                {
+                    numberOfPairs++;
+                    pairs.Remove(ar[i]);
+                }
+            }
+
+            return numberOfPairs;
+        }
+    }
+}
