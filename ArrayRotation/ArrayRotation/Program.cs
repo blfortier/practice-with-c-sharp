09a7612b33ad80ddb06ebd9183c31dca59d9a8a7
Brittney Fortier <brittfort34@gmail.com>
2019-04-25 15:04:17 -0400

    Refactored, created a method and added some writeline statements.

--- ArrayRotation/ArrayRotation/Program.cs
+++ ArrayRotation/ArrayRotation/Program.cs
@@ -12,28 +12,41 @@ namespace ArrayRotation
         {
             int[] origArr = new int[6] { 1, 2, 3, 4, 5, 6};
           
-            int maxIndex = origArr.Length - 1;
 
             Console.Write("Rotate array left by how many: ");
             var rotateLeftBy = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine();
 
-            for (int i = 0; i < rotateLeftBy; i++)
-            {
-                int temp = origArr[0];
-
-                for (int j = 0; j < maxIndex; j++)
-                    origArr[j] = origArr[j + 1];
-
-                origArr[maxIndex] = temp;
-            }
-
-            Console.WriteLine("Rotated array: ");
+            Console.WriteLine("Original array: ");
             foreach (var item in origArr)
             {
                 Console.Write(item);
             }
             Console.WriteLine();
+
+            Console.WriteLine("Rotated array: ");
+            foreach (var item in RotateArray(origArr, rotateLeftBy))
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+
+        public static int[] RotateArray(int[] arr, int numSpacesToMove)
+        {
+            int maxIndex = arr.Length - 1;
+
+            for (int i = 0; i < numSpacesToMove; i++)
+            {
+                int temp = arr[0];
+
+                for (int j = 0; j < maxIndex; j++)
+                    arr[j] = arr[j + 1];
+
+                arr[maxIndex] = temp;
+            }
+
+            return arr;
         }
     }
 }
