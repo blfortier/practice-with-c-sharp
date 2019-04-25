f75cef0508406ac4850b2bf66fe00114ef1a840b
Brittney Fortier <brittfort34@gmail.com>
2019-04-25 14:55:03 -0400

    Shifts the elements of an array to the left by n spaces, n given by user.

---
+++ ArrayRotation/ArrayRotation/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayRotation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] origArr = new int[6] { 1, 2, 3, 4, 5, 6};
+          
+            int maxIndex = origArr.Length - 1;
+
+            Console.Write("Rotate array left by how many: ");
+            var rotateLeftBy = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for (int i = 0; i < rotateLeftBy; i++)
+            {
+                int temp = origArr[0];
+
+                for (int j = 0; j < maxIndex; j++)
+                    origArr[j] = origArr[j + 1];
+
+                origArr[maxIndex] = temp;
+            }
+
+            Console.WriteLine("Rotated array: ");
+            foreach (var item in origArr)
+            {
+                Console.Write(item);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ EquilibriumIndex/EquilibriumIndex/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EquilibriumIndex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many elements in array: ");
+            int numElements = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter array of integers: ");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+       
+            
+        }
+
+        public static int FindEquilibriumIndex(int n, int[] arr)
+        {
+            int middle = n / 2;
+
+            if (n % 2 == 0)
+                middle
+            else
+                middle = arr[middle];
+            for (int i = 0;)
+            return -1;
+        }
+    }
+}
