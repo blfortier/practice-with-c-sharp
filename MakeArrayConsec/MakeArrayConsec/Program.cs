6bd56eaa82d9104b27ef3f93ace1963a9cc9fb3c
Brittney Fortier <brittfort34@gmail.com>
2019-05-05 16:20:33 -0400

    Given an int array, returns the number of elements that would need to be added in order to make the array consecutive.

---
+++ MakeArrayConsec/MakeArrayConsec/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MakeArrayConsec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many integers in array: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the integers in the array: ");
+            int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+            Console.WriteLine("You would need to add {0} numbers to make the array consecutive", MakeArrayConsecutive(arr));            
+        }
+
+        public static int MakeArrayConsecutive(int[] statues)
+        {
+            Array.Sort(statues);
+            int count = 0;
+            int offset = 0;
+
+            for (int i = 0; i < statues.Length - 1; i++)
+            {
+                if (statues[i + 1] != statues[i] + 1)
+                {
+                    count += statues[i + 1] - statues[i];
+                    offset++;
+                }
+            }
+
+            return count - offset;
+        }
+    }
+}
