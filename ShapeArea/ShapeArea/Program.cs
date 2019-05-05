76782e8c032d96eb64478542a75fc9f47af51b5e
Brittney Fortier <brittfort34@gmail.com>
2019-05-04 21:28:07 -0400

    Finds the area of an interesting polygon using a simple math equation.

---
+++ ShapeArea/ShapeArea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What size interesting polygon: ");
+            int size = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The total area is: {0}", FindshapeArea(size));
+        }
+
+        public static int FindshapeArea(int n)
+        {
+            return (int)(n * n + (Math.Pow(n, 2) - 2 * n + 1));
+        }
+    }
+}
---
+++ VerifySequenceOrder/VerifySequenceOrder/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VerifySequenceOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public bool launchSequenceChecker(string[] systemNames, int[] stepNumbers)
+        {
+            List<int> nums = new List<int>();
+            List<string> names = new List<string>();
+            
+            for (int i = 0; i < systemNames.Length; i++)
+            {
+                names.Add(systemNames[i]);
+
+                for (int j = 0; j < stepNumbers.Length; j++)
+                {
+                    if (names.Contains(systemNames[i]))
+                    {                        
+                        nums.Add(stepNumbers[j]);
+                    }
+                }
+            }
+
+
+
+            return false;
+        }
+
+    }
+}
