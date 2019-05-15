ecb8bb05b9c9b17f3da9b67a9221d64d5c70280f
Brittney Fortier <brittfort34@gmail.com>
2019-05-14 21:01:29 -0400

    Reads from the console 2 ints, a divisor and a bound. Returns the largest integer N where N is divisible by the divisor, N is less than or equal to bound, and N is greater than 0.

---
+++ MaxMultiple/MaxMultiple/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxMultiple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Given a divisor and a bound, find the largest integer N such that:
+             * N is divisible by divisor.
+             * N is less than or equal to bound.
+             * N is greater than 0.
+             */
+
+            Console.Write("What's the divisor (int)? ");
+            int divisor = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What's the bound (int)? " );
+            int bound = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The max multiple is {0}.", MaxMultiple(divisor, bound));
+        }
+
+        public static int MaxMultiple(int divisor, int bound)
+        {
+            return bound - (bound % divisor);
+        }
+    }
+}
