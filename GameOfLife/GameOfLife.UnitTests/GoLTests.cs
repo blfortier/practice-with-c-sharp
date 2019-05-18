60c2054f116a6fd5b311ca4d504c1f6d531554d1
Brittney Fortier <brittfort34@gmail.com>
2019-05-18 14:08:04 -0400

    Created a data structure to store the state of the board.

---
+++ GameOfLife/GameOfLife.UnitTests/GoLTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+
+namespace GameOfLife.UnitTests
+{
+    [TestFixture]
+    public class GoLTests
+    {
+        private GameBoard _gameBoard;
+
+        [SetUp]
+        public void TestFixtureSetup()
+        {
+            _gameBoard = new GameBoard();
+            _gameBoard.SetInitialState();
+        }
+
+        [Test]
+        public void InsertCharIntoOneCellOfBoard()
+        {
+            int row = 1; 
+            int col = 2;
+
+            var expected = 'X';
+            var actual = _gameBoard.InsertCharIntoCell(row, col);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void ChooseOneCell_ReturnEmptyBoard()
+        {
+            int row = 1;
+            int col = 2;
+
+            var expected = 'X';
+            var actual = _gameBoard.InsertCharIntoCell(row, col);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+    }
+}
---
+++ GameOfLife/GameOfLife/GameBoard.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameOfLife
+{
+    public class GameBoard
+    {
+        public char[,] boardState = new char[5, 5];
+
+        public void SetInitialState()
+        {
+            for (int row = 0; row < boardState.GetLength(1); row++)
+            {
+                for (int col = 0; col < boardState.GetLength(0); col++)
+                {
+                    boardState[row, col] = 'X';
+                }
+            }
+        }
+
+        public void DisplayBoard()
+        {
+            for (int row = 0; row < boardState.GetLength(1); row++)
+            {
+                string row1 = "| ";
+                string row2 = "";
+
+                for (int col = 0; col < boardState.GetLength(0); col++)
+                {
+                    row1 = row1 + boardState[row, col] + " | ";
+                    row2 += "+---";
+                }
+                Console.WriteLine(row1);
+                Console.WriteLine(row2 + "+");
+            }
+        }
+
+        public char InsertCharIntoCell(int row, int col)
+        {
+            return boardState[row, col] = 'X';
+        }
+    }
+}
+
