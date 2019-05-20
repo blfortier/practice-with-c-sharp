ac934abd1c5d3a6aabe6caa4f9dbae90de4e66fd
Brittney Fortier <brittfort34@gmail.com>
2019-05-19 22:33:11 -0400

    Calculates the time based on minutes that have elapsed on a timer. Returns an answer as the sum of digits that the timer in the format hh:mm would show.

---
+++ AnagramChecker/AnagramChecker/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnagramChecker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dict = new Dictionary<string, int>();
+            string path = @"C:\Users\Owner\source\repos\practice-with-c-sharp\AnagramChecker\AnagramChecker\Dictionary.txt";
+
+            if (!File.Exists(path))
+                Console.WriteLine("Error reading file.");
+
+            string[] readText = File.ReadAllText(path).Split();
+
+            // string sorted = String.Concat(readText.OrderBy(c => c));
+            string sorted = "";
+            List<int> indexes = new List<int>();
+
+            for (int index =  0; index < readText.Length; index++)
+            {
+                sorted = SortString(readText[index]);
+                if (!dict.ContainsKey(sorted))
+                    dict.Add(sorted, 1);
+                else
+                {
+                    dict[sorted]++;
+                    indexes.Add(index);
+                }
+            }
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item);
+            }
+
+          // PrintIndexAndKeysAndValues(ht);
+            foreach (var item in indexes)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+        public static string SortString(string input)
+        {
+            char[] chars = input.ToArray();
+            Array.Sort(chars);
+            return new string(chars);
+        }
+
+        public static void PrintIndexAndKeysAndValues(Hashtable myHT)
+        {
+            int i = 0;
+            Console.WriteLine("\t-INDEX-\t-KEY-\t-VALUE-");
+            foreach (DictionaryEntry de in myHT)
+                Console.WriteLine("\t[{0}]:\t{1}\t{2}", i++, de.Key, de.Value);
+            Console.WriteLine();
+        }
+    }
+}
---
+++ LateRide/LateRide/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LateRide
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("How many minutes have elapsed on the timer? ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            LateRide(n);
+        }
+
+        public static int LateRide(int n)
+        {
+            int hoursPassed = n / 60;
+            int minutesPassed = n % 60;
+             
+            string join = hoursPassed.ToString() + minutesPassed.ToString();
+
+            int sum = 0;
+
+            for (int i = 0; i < join.Length; i++)
+                sum += Int32.Parse(join[i].ToString());
+
+            return sum;
+        }
+    }
+}
