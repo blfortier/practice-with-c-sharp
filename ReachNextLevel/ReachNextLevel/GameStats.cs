efd6541c8d83852a8b51b7a3df115b3ae3057aec
Brittney Fortier <brittfort34@gmail.com>
2019-05-22 15:38:25 -0400

    Determines if a player wll advance to the next level after killing a monster.

---
+++ ReachNextLevel/ReachNextLevel/GameStats.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReachNextLevel
+{
+    public class GameStats
+    {
+        private int _exp;
+        private int _threshold;
+        private int _reward;
+
+        public int Exp { get => _exp; set => _exp = value; }
+        public int Threshold { get => _threshold; set => _threshold = value; }
+        public int Reward { get => _reward; set => _reward = value; }
+
+        public GameStats(int xp, int toNextLevel, int reward)
+        {
+            this.Exp = xp;
+            this.Threshold = toNextLevel;
+            this.Reward = reward;
+        }
+
+        public bool ReachNextLevel(GameStats info)
+        {
+            if (this.Exp >= this.Threshold)
+                return true;
+            else
+            {
+                if (this.Exp + this.Reward >= this.Threshold)
+                    return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ ReachNextLevel/ReachNextLevel/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReachNextLevel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("What is the experience level? ");
+            int experience = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What is the threshold? ");
+            int nextLevel = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("What's the reward for killing the monster? ");
+            int reward = Convert.ToInt32(Console.ReadLine());
+
+            GameStats info = new GameStats(experience, nextLevel, reward);
+
+            string message = (info.ReachNextLevel(info)) ? "You will advance to the next level!" :
+                                "You will not advance to the next level.";
+
+            Console.WriteLine(message);
+        }
+    }
+}
