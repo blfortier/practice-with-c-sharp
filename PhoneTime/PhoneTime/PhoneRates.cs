7e376edb145ccc68fe21b80b4f7155478c048fc4
Brittney Fortier <brittfort34@gmail.com>
2019-05-21 21:15:01 -0400

    Calculates the total time somebody can talk on the phone based on phone rates and the amount in their account to begin with.

---
+++ PhoneTime/PhoneTime/PhoneRates.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneTime
+{
+    public class PhoneRates
+    {
+        private int _firstMin;
+        private int _mins2_10;
+        private int _min11;
+
+        public int FirstMin{ get => _firstMin ; set => _firstMin = value; }
+        public int Mins2_10 { get => _mins2_10; set => _mins2_10 = value; }
+        public int Min11 { get => _min11; set => _min11 = value; }
+
+        public PhoneRates(int firstMin, int mins2To10, int mins11)
+        {
+            this._firstMin = firstMin;
+            this._mins2_10 = mins2To10;
+            this._min11 = mins11;
+        }
+    }
+}
---
+++ PhoneTime/PhoneTime/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PhoneTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Rates for first minute: ");
+            int input1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Rates for minutes 2 through 10: ");
+            int input2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Rates for minutes 11 and on: ");
+            int input3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("How many cents in your account? ");
+            int cents = Convert.ToInt32(Console.ReadLine());
+
+            PhoneRates rates = new PhoneRates(input1, input2, input3);
+            Console.WriteLine("You can talk for {0} minutes.", PhoneCall(rates, cents));
+        }
+
+        public static int PhoneCall(PhoneRates rates, int centsInAccount)
+        {
+            int totalMinutes = 1;
+
+            if (centsInAccount >= rates.FirstMin)
+                centsInAccount -= rates.FirstMin;
+            else
+                return 0;
+
+            if (rates.Mins2_10 * 9 < centsInAccount)
+            {
+                centsInAccount -= rates.Mins2_10 * 9;
+                totalMinutes += (centsInAccount / rates.Min11) + 9;
+                return totalMinutes;
+            }
+            else
+                totalMinutes += (centsInAccount / rates.Mins2_10);
+
+            return totalMinutes;
+
+        }
+    }
+}
