3009c99612b566d9a8a3ce625c6606b92ff20cfe
Brittney Fortier <brittfort34@gmail.com>
2019-06-09 23:03:10 -0400

    Created some more tests.

--- ConnectFour/ConnectFour.UnitTests/ConnectFourTests.cs
+++ ConnectFour/ConnectFour.UnitTests/ConnectFourTests.cs
@@ -12,16 +12,64 @@ namespace ConnectFour.UnitTests
     [TestFixture]
     public class ConnectFourTests
     {
-        public Colors[,] _board = new Colors[6, 6];
+        public Colors[,] _board;
+        public int Rows = 6;
+        public int Columns = 6;
 
         Colors Gray = Colors.Gray;
         Colors Red = Colors.Red;
         Colors Blue = Colors.Blue;
 
+        [Test]
+        public void GetStateOfOneCell()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[4, 5] = Blue;
+            _board[0, 0] = Red;
+
+            DisplayBoard(_board);
+
+            var expected = Blue;
+            var actual = GetStateOfCell(_board, 4, 5);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void CheckACellForOneNeighborAbove()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[3, 2] = Blue;
+            _board[4, 2] = Blue;
+
+            DisplayBoard(_board);
+
+            var expected = true;
+            var actual = CheckCellForNeighborsAbove(_board);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        //[Test]
+        //public void CheckACellForOneNeighborBelow()
+        //{
+        //    _board = new Colors[Rows, Columns];
+        //    _board[3, 2] = Blue;
+        //    _board[2, 2] = Blue;
+
+        //    DisplayBoard(_board);
+
+        //    var expected = true;
+        //    var actual = CheckCellForNeighborsAbove(_board);
+
+        //    Assert.AreEqual(expected, actual);
+        //}
+
 
         [Test]
         public void CheckACellForOneNeighborToTheRight()
         {
+            _board = new Colors[Rows, Columns];
             _board[3, 2] = Blue;
             _board[3, 3] = Blue;
 
@@ -36,6 +84,7 @@ namespace ConnectFour.UnitTests
         [Test]
         public void CheckACellForOneNeighborToTheLeft()
         {
+            _board = new Colors[Rows, Columns];
             _board[1, 4] = Blue;
             _board[1, 3] = Blue;
 
@@ -50,6 +99,7 @@ namespace ConnectFour.UnitTests
         [Test]
         public void CheckACellForOneNeighborToLeftDiagUpper()
         {
+            _board = new Colors[Rows, Columns];
             _board[2, 2] = Blue;
             _board[1, 1] = Blue;
 
@@ -62,22 +112,129 @@ namespace ConnectFour.UnitTests
         }
 
         [Test]
-        public void CheckACellForOneNeighborToLeftDiagLower()
+        public void SeeIfThereIsAVerticalWinFromBottomOfGrid()
         {
-            _board[2, 2] = Red;
-            _board[3, 1] = Red;
+            _board = new Colors[Rows, Columns];
+            _board[5, 2] = Blue;
+            _board[4, 2] = Blue;
+            _board[3, 2] = Blue;
+            _board[2, 2] = Blue;
 
             DisplayBoard(_board);
 
             var expected = true;
-            var actual = CheckCellForNeighborsToLeftDiagLower(_board);
+            var actual = HasVerticalWin(_board, 5, 2);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+
+        [Test]
+        public void SeeIfThereIsAVerticalWinFromBottomSecondToLastRowOfGrid()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[4, 2] = Red;
+            _board[2, 2] = Red;
+            _board[3, 2] = Red;
+            _board[1, 2] = Red;
+
+            DisplayBoard(_board);
+
+            var expected = true;
+            var actual = HasVerticalWin(_board, 4, 2);
+
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [Test]
+        public void CheckForAHorizontalWinFromBottomOfGrid()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[5, 3] = Red;
+            _board[5, 2] = Red;
+            _board[5, 1] = Red;
+            _board[5, 0] = Red;
+
+            DisplayBoard(_board);
+
+            var expected = true;
+            var actual = HasHorizontalWin(_board, 5, 3);
 
             Assert.AreEqual(expected, actual);
         }
 
         [Test]
+        public void WhenTheColumnIsTwoOrLess_DoNotCheckForHoriZontalWin()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[5, 2] = Red;
+            _board[5, 1] = Red;
+            _board[5, 0] = Red;
+
+            DisplayBoard(_board);
+
+            var expected = false;
+            var actual = HasHorizontalWin(_board, 5, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void WhenTheRowIsTwoOrLess_DoNotCheckForVerticalWin()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[5, 2] = Red;
+            _board[5, 1] = Red;
+            _board[5, 0] = Red;
+
+            DisplayBoard(_board);
+
+            var expected = false;
+            var actual = HasHorizontalWin(_board, 5, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
+        public void WhenTheColumnIsThreeAwayFromRightEdgeOfBoard_DoNotCheckForDiagonalRightWin()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[5, 3] = Blue;
+            _board[4, 4] = Blue;
+            _board[3, 5] = Blue;
+
+            DisplayBoard(_board);
+
+            var expected = false;
+            var actual = HasDiagonalWin(_board, 5, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void CheckForDiagonalWin()
+        {
+            _board = new Colors[Rows, Columns];
+            _board[4, 2] = Red;
+            _board[3, 3] = Red;
+            _board[2, 4] = Red;
+            _board[1, 5] = Red;
+
+            DisplayBoard(_board);
+
+            var expected = true;
+            var actual = HasDiagonalWin(_board, 4, 2);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+
+        [Test]
         public void CheckACellForOneNeighborToRightDiagUpper()
         {
+            _board = new Colors[Rows, Columns];
             _board[3, 2] = Red;
             _board[2, 3] = Red;
 
@@ -89,18 +246,40 @@ namespace ConnectFour.UnitTests
             Assert.AreEqual(expected, actual);
         }
 
-        [Test]
-        public void CheckACellForOneNeighborToRightDiagLower()
-        {
-            _board[3, 2] = Red;
-            _board[4, 3] = Red;
 
-            DisplayBoard(_board);
 
-            var expected = true;
-            var actual = CheckCellForNeighborsToRightDiagLower(_board);
 
-            Assert.AreEqual(expected, actual);
-        }
+
+
+        //[Test]
+        //public void CheckACellForOneNeighborToRightDiagLower()
+        //{
+        //    _board = new Colors[Rows, Columns];
+        //    _board[3, 2] = Red;
+        //    _board[4, 3] = Red;
+
+        //    DisplayBoard(_board);
+
+        //    var expected = true;
+        //    var actual = CheckCellForNeighborsToRightDiagLower(_board);
+
+        //    Assert.AreEqual(expected, actual);
+        //}
+
+        //[Test]
+        //public void CheckACellForOneNeighborToLeftDiagLower()
+        //{
+        //    _board = new Colors[Rows, Columns];
+        //    _board[2, 2] = Red;
+        //    _board[3, 1] = Red;
+
+        //    DisplayBoard(_board);
+
+        //    var expected = true;
+        //    var actual = CheckCellForNeighborsToLeftDiagLower(_board);
+
+        //    Assert.AreEqual(expected, actual);
+        //}
+
     }
 }
--- ConnectFour/ConnectFour/Board.cs
+++ ConnectFour/ConnectFour/Board.cs
@@ -12,13 +12,9 @@ namespace ConnectFour
         private int _columns;
         public Colors[,] board;
 
-        Colors Blue = Colors.Blue;
-        Colors Gray = Colors.Gray;
-        Colors Red = Colors.Red;
-
         public int Rows { get => _rows; set => _rows = value; }
         public int Columns { get => _columns; set => _columns = value; }
-    
+
         public Board(int numOfRows, int numOfColumns)
         {
             this.Rows = numOfRows;
@@ -58,10 +54,56 @@ namespace ConnectFour
             }
         }
 
+        public static Colors GetStateOfCell(Colors[,] board, int row, int col)
+        {
+            return board[row, col];
+        }
+
+
+        public static bool CheckCellForNeighborsAbove(Colors[,] board)
+        {
+            Colors Blue = Colors.Blue;
+            Colors Red = Colors.Red;
+
+            for (int row = 0; row < board.GetLength(0); row++)
+            {
+                for (int col = 0; col < board.GetLength(1); col++)
+                {
+                    if (board[row, col] == Blue && board[row + 1, col] == Blue)
+                        return true;
+
+                    if (board[row, col] == Red && board[row + 1, col] == Red)
+                        return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        //public static bool CheckCellForNeighborsBelow(Colors[,] board)
+        //{
+        //    Colors Blue = Colors.Blue;
+        //    Colors Red = Colors.Red;
+
+        //    for (int row = 0; row < board.GetLength(0); row++)
+        //    {
+        //        for (int col = 0; col < board.GetLength(1); col++)
+        //        {
+        //            if (board[row, col] == Blue && board[row - 1, col] == Blue)
+        //                return true;
+
+        //            if (board[row, col] == Red && board[row - 1, col] == Red)
+        //                return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+
         public static bool CheckCellForNeighborsToRight(Colors[,] board)
         {
             Colors Blue = Colors.Blue;
-            Colors Gray = Colors.Gray;
             Colors Red = Colors.Red;
 
             for (int row = 0; row < board.GetLength(0); row++)
@@ -83,7 +125,6 @@ namespace ConnectFour
         public static bool CheckCellForNeighborsToLeft(Colors[,] board)
         {
             Colors Blue = Colors.Blue;
-            Colors Gray = Colors.Gray;
             Colors Red = Colors.Red;
 
             for (int row = 0; row < board.GetLength(0); row++)
@@ -104,7 +145,6 @@ namespace ConnectFour
         public static bool CheckCellForNeighborsToLeftDiagUpper(Colors[,] board)
         {
             Colors Blue = Colors.Blue;
-            Colors Gray = Colors.Gray;
             Colors Red = Colors.Red;
 
             for (int row = 0; row < board.GetLength(0); row++)
@@ -122,31 +162,29 @@ namespace ConnectFour
             return false;
         }
 
-        public static bool CheckCellForNeighborsToLeftDiagLower(Colors[,] board)
-        {
-            Colors Blue = Colors.Blue;
-            Colors Gray = Colors.Gray;
-            Colors Red = Colors.Red;
+        //public static bool CheckCellForNeighborsToLeftDiagLower(Colors[,] board)
+        //{
+        //    Colors Blue = Colors.Blue;
+        //    Colors Red = Colors.Red;
 
-            for (int row = 0; row < board.GetLength(0); row++)
-            {
-                for (int col = 0; col < board.GetLength(1); col++)
-                {
-                    if (board[row, col] == Blue && board[row + 1, col - 1] == Blue)
-                        return true;
+        //    for (int row = 0; row < board.GetLength(0); row++)
+        //    {
+        //        for (int col = 0; col < board.GetLength(1); col++)
+        //        {
+        //            if (board[row, col] == Blue && board[row + 1, col - 1] == Blue)
+        //                return true;
 
-                    if (board[row, col] == Red && board[row + 1, col - 1] == Red)
-                        return true;
-                }
-            }
+        //            if (board[row, col] == Red && board[row + 1, col - 1] == Red)
+        //                return true;
+        //        }
+        //    }
 
-            return false;
-        }
+        //    return false;
+        //}
 
         public static bool CheckCellForNeighborsToRightDiagUpper(Colors[,] board)
         {
             Colors Blue = Colors.Blue;
-            Colors Gray = Colors.Gray;
             Colors Red = Colors.Red;
 
             for (int row = 0; row < board.GetLength(0); row++)
@@ -164,25 +202,291 @@ namespace ConnectFour
             return false;
         }
 
-        public static bool CheckCellForNeighborsToRightDiagLower(Colors[,] board)
+        //public static bool CheckCellForNeighborsToRightDiagLower(Colors[,] board)
+        //{
+        //    Colors Blue = Colors.Blue;
+        //    Colors Red = Colors.Red;
+
+        //    for (int row = 0; row < board.GetLength(0); row++)
+        //    {
+        //        for (int col = 0; col < board.GetLength(1); col++)
+        //        {
+        //            if (board[row, col] == Blue && board[row + 1, col + 1] == Blue)
+        //                return true;
+
+        //            if (board[row, col] == Red && board[row + 1, col + 1] == Red)
+        //                return true;
+        //        }
+        //    }
+
+        //    return false;
+        //}
+
+        //public int CountHorizontalNeighborsOfCell(int row, int col)
+        //{
+        //    int horizontalNeighborCount = 0;
+
+        //    if (col == 0)
+        //    {
+        //        if (boardState[row, col + 1] == true)
+        //            horizontalNeighborCount++;
+        //    }
+        //    else if (col == _lastColumn)
+        //    { 
+        //        if (boardState[row, col - 1] == true)
+        //            horizontalNeighborCount++;
+        //    }
+        //    else
+        //    {
+        //        // horizontal
+        //        if (boardState[row, col - 1] == true)
+        //            horizontalNeighborCount++;
+        //        if (boardState[row, col + 1] == true)
+        //            horizontalNeighborCount++;
+        //    }
+
+        //    return horizontalNeighborCount;
+        //}
+
+        //public int CountVerticalNeighborsOfCell(int row, int col)
+        //{
+        //    int verticalNeighborCount = 0;
+
+        //    if (row == 0)
+        //    {
+        //        if (boardState[row + 1, col] == true)
+        //            verticalNeighborCount++;
+        //    }
+        //    else if (row == _lastRow)
+        //    {
+        //        if (boardState[row - 1, col] == true)
+        //            verticalNeighborCount++;
+        //    }
+        //    else
+        //    {
+        //        // vertical
+        //        if (boardState[row - 1, col] == true)
+        //            verticalNeighborCount++;
+        //        if (boardState[row + 1, col] == true)
+        //            verticalNeighborCount++;
+        //    }
+
+        //    return verticalNeighborCount;
+        //}
+
+
+        //public int CountDiagonalNeighorsOfCell(int row, int col)
+        //{
+        //    int diagonalNeighborCount = 0;
+        //    int lastColumn = _lastColumn;
+        //    int lastRow = _lastRow;
+
+        //    if (col == 0)
+        //    {
+        //        if (row == 0)
+        //        {
+        //            if (boardState[row + 1, col + 1] == true)
+        //                diagonalNeighborCount++;
+        //        }
+        //        else if (row == _lastRow)
+        //        {
+        //            if (boardState[row - 1, col + 1] == true)
+        //                diagonalNeighborCount++;
+        //        }
+        //        else
+        //        {
+        //            if (boardState[row - 1, col + 1] == true)
+        //                diagonalNeighborCount++;
+        //            if (boardState[row + 1, col + 1] == true)
+        //                diagonalNeighborCount++;
+
+        //        }
+        //    }
+        //    else if (col == _lastColumn)
+        //    {
+        //        if (row == 0)
+        //        {
+        //            if (boardState[row + 1, col - 1])
+        //                diagonalNeighborCount++;
+        //        }
+        //        else if (row == _lastRow)
+        //        {
+        //            if (boardState[row - 1, col - 1] == true)
+        //                diagonalNeighborCount++;
+        //        }
+        //        else
+        //        {
+        //            if (boardState[row - 1, col - 1] == true)
+        //                diagonalNeighborCount++;
+        //            if (boardState[row + 1, col - 1] == true)
+        //                diagonalNeighborCount++;
+        //        }
+        //    }
+        //    else if (row == 0)
+        //    {
+
+        //        if (boardState[row + 1, col + 1] == true)
+        //            diagonalNeighborCount++;
+        //        if (boardState[row + 1, col - 1] == true)
+        //            diagonalNeighborCount++;
+        //    }
+        //    else if (row == _lastRow)
+        //    {
+        //        if (boardState[row - 1, col - 1] == true)
+        //            diagonalNeighborCount++;
+        //        if (boardState[row - 1, col + 1] == true)
+        //            diagonalNeighborCount++;
+        //    }
+        //    else
+        //    {
+        //        // diag left
+        //        if (boardState[row - 1, col - 1] == true)
+        //            diagonalNeighborCount++;
+        //        if (boardState[row + 1, col + 1] == true)
+        //            diagonalNeighborCount++;
+
+        //        // diag right
+        //        if (boardState[row - 1, col + 1] == true)
+        //            diagonalNeighborCount++;
+        //        if (boardState[row + 1, col - 1] == true)
+        //            diagonalNeighborCount++;
+        //    }
+
+        //    return diagonalNeighborCount;
+        //}
+
+        //public int CountActiveNeighborsOfCell(int row, int col)
+        //{
+        //    int vertical = CountVerticalNeighborsOfCell(row, col);
+        //    int horizontal = CountHorizontalNeighborsOfCell(row, col);
+        //    int diagonal = CountDiagonalNeighorsOfCell(row, col);
+
+        //    neighborCount = vertical + horizontal + diagonal;
+
+        //    return neighborCount;
+        //}
+
+
+        public static bool HasVerticalWin(Colors[,] board, int row, int col)
         {
-            Colors Blue = Colors.Blue;
-            Colors Gray = Colors.Gray;
-            Colors Red = Colors.Red;
+            Colors state = board[row, col];
 
-            for (int row = 0; row < board.GetLength(0); row++)
+            if (board[row - 1, col] == state && board[row - 2, col] == state && board[row - 3, col] == state)
+                return true;
+            else
+                return false;
+
+        }
+
+        public static bool HasHorizontalWin(Colors[,] board, int row, int col)
+        {
+            Colors state = board[row, col];
+
+            if (col > 2)
             {
-                for (int col = 0; col < board.GetLength(1); col++)
-                {
-                    if (board[row, col] == Blue && board[row + 1, col + 1] == Blue)
-                        return true;
-
-                    if (board[row, col] == Red && board[row + 1, col + 1] == Red)
-                        return true;
-                }
+                if (board[row, col - 1] == state && board[row, col - 2] == state && board[row, col - 3] == state)
+                    return true;
             }
 
             return false;
+        }
+
+        public static bool HasDiagonalWin(Colors[,] board, int row, int col)
+        {
+            Colors state = board[row, col];
+
+            if (col <= 2 && row > 2)
+            {
+                if (board[row - 1, col + 1] == state && board[row - 2, col + 2] == state && board[row - 3, col + 3] == state)
+                    return true;
+            }
+            else if (col >= 3 && row > 2)
+            {
+                if (board[row - 1, col - 1] == state && board[row - 2, col - 2] == state && board[row - 3, col - 3] == state)
+                    return true;
+            }
+            else if (row)
+
+            return false;
+
+            //    int diagonalNeighborCount = 0;
+            //    int lastColumn = board.GetLength(0);
+            //    int lastRow = board.GetLength(1);
+
+            //    if (col == 0)
+            //    {
+            //        if (row == 0)
+            //        {
+            //            if (board[row + 1, col + 1] == state)
+            //                diagonalNeighborCount++;
+            //        }
+            //        else if (row == lastRow)
+            //        {
+            //            if (board[row - 1, col + 1] == state)
+            //                diagonalNeighborCount++;
+            //        }
+            //        else
+            //        {
+            //            if (board[row - 1, col + 1] == state)
+            //                diagonalNeighborCount++;
+            //            if (board[row + 1, col + 1] == state)
+            //                diagonalNeighborCount++;
+
+            //        }
+            //    }
+            //    else if (col == lastColumn)
+            //    {
+            //        if (row == 0)
+            //        {
+            //            if (board[row + 1, col - 1] == state)
+            //                diagonalNeighborCount++;
+            //        }
+            //        else if (row == lastRow)
+            //        {
+            //            if (board[row - 1, col - 1] == state)
+            //                diagonalNeighborCount++;
+            //        }
+            //        else
+            //        {
+            //            if (board[row - 1, col - 1] == state)
+            //                diagonalNeighborCount++;
+            //            if (board[row + 1, col - 1] == state)
+            //                diagonalNeighborCount++;
+            //        }
+            //    }
+            //    else if (row == 0)
+            //    {
+
+            //        if (board[row + 1, col + 1] == state)
+            //            diagonalNeighborCount++;
+            //        if (board[row + 1, col - 1] == state)
+            //            diagonalNeighborCount++;
+            //    }
+            //    else if (row == lastRow)
+            //    {
+            //        if (board[row - 1, col - 1] == state)
+            //            diagonalNeighborCount++;
+            //        if (board[row - 1, col + 1] == state)
+            //            diagonalNeighborCount++;
+            //    }
+            //    else
+            //    {
+            //        // diag left
+            //        if (board[row - 1, col - 1] == state)
+            //            diagonalNeighborCount++;
+            //        if (board[row + 1, col + 1] == state)
+            //            diagonalNeighborCount++;
+
+            //        // diag right
+            //        if (board[row - 1, col + 1] == state)
+            //            diagonalNeighborCount++;
+            //        if (board[row + 1, col - 1] == state)
+            //            diagonalNeighborCount++;
+            //    }
+
+
+            //return (diagonalNeighborCount >= 4) ? true : false;
+
         }
     }
 }
